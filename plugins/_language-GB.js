a8eb159c14f93951559d0acd497b7318fa830bbf
nixonvidal <147317708+nixonvidal@users.noreply.github.com>
2024-03-29 12:53:28 -0500

    Add files via upload

---
+++ plugins/_language-GB.js
@@ -0,0 +1,21 @@
+import { en, es, id, ar, pt } from '../lib/idiomas/total-idiomas.js'  
+export async function before(m, { conn }) {
+global.wait = lenguajeGB['smsMeWait']()
+global.wait2 = lenguajeGB.smsWait()
+  
+let user = global.db.data.users[m.sender]
+
+if (user.GBLanguage == 'es') {
+global.lenguajeGB = es
+} else if (user.GBLanguage == 'en') {
+global.lenguajeGB = en
+} else if (user.GBLanguage == 'pt') {
+global.lenguajeGB = pt
+} else if (user.GBLanguage == 'ar') {
+global.lenguajeGB = ar
+} else if (user.GBLanguage == 'id') {
+global.lenguajeGB = id
+} else {
+global.lenguajeGB = es
+}
+}
